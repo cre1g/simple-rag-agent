d54a4c33002f91e7d3c3810047ba58cf85da8af1
cre1g <creigcooke@gmail.com>
2026-03-07 23:12:20 -0500

    Built ingest.py to chunk and embed Updated requirements.txt Updated gitignore to ignore chroma_db

---
+++ ingest.py
@@ -0,0 +1,27 @@
+import os
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+loader = DirectoryLoader("./docs", glob="./*.txt", loader_cls=TextLoader)
+
+raw_docs = loader.load()
+
+print(f"Loaded {len(raw_docs)} separate files.")
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=20
+)
+
+all_chunks = splitter.split_documents(raw_docs)
+
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+Chroma.from_documents(all_chunks, embedding_model, persist_directory="./chroma_db")
+
+print("Database created successfully!")
