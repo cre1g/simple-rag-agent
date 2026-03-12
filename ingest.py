4dba133d0fb75e966cfec2b7a47242dd61cbc17e
cre1g <creigcooke@gmail.com>
2026-03-11 22:52:04 -0400

    Fixed deprecated chroma import

--- ingest.py
+++ ingest.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
